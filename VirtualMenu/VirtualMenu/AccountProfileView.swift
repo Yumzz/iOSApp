e1398c98fefdea809b4ade9eb3b92649b6c09e0f
RohoCodo <rtrocks722@gmail.com>
2020-06-20 14:30:44 -0700

    Profile Page

--- VirtualMenu/VirtualMenu/AccountProfileView.swift
+++ VirtualMenu/VirtualMenu/AccountProfileView.swift
@@ -33,23 +33,46 @@ class AccountProfileViewModel: ObservableObject {
     @Published var user: ARMUser? = nil
     @Published var username: String? = nil
     
-    let dbb = DatabaseRequest()
+    let dbb2 = DatabaseRequest()
     
     func fetchUser() {
+        var fetchUser: ARMUser? = nil
         DispatchQueue.main.async {
-            self.user = db.fetchUserWithCloudID(cloudID: KeychainItem.currentUserIdentifier!)
+            fetchUser = self.dbb2.fetchUserWithCloudID(cloudID: KeychainItem.currentUserIdentifier!)
+            self.user = fetchUser
             self.username = self.user?.userName
         }
     }
     
-    func getProfPhoto() -> UIImage? {
-        if self.user?.profilePhoto != nil {
-            let imageAsset: CKAsset? = self.user?.profilePhoto
+    func getProfPhoto() -> UIImage {
+        print("gettin photo")
+        if self.user != nil {
+            let imageAsset: CKAsset? = self.user!.profilePhoto
             let data = NSData(contentsOf: (imageAsset?.fileURL!)!)
             let image = UIImage(data: data! as Data)
-            return image
+            if image == nil {
+                print("nil image but user is there")
+                return UIImage(imageLiteralResourceName: "profile_photo_edit")
+            }
+            else{
+                print("image exists")
+                return image!
+            }
         }
         else{
+            print("user does not exist")
+            return UIImage(imageLiteralResourceName: "profile_photo_edit")
+        }
+    }
+    
+    func getUIImageFromCKAsset(image: CKAsset?) -> UIImage? {
+        if(self.user != nil){
+            let file: CKAsset? = image
+            let data = NSData(contentsOf: (file?.fileURL!)!)
+        
+            return UIImage(data: data! as Data) ?? nil
+        }
+        else {
             return UIImage(imageLiteralResourceName: "profile_photo_edit")
         }
     }
@@ -59,42 +82,76 @@ class AccountProfileViewModel: ObservableObject {
 struct AccountProfileView: View {
     
     @ObservedObject var AccountProf = AccountProfileViewModel()
-    
+    @State private var showingImagePicker = false
+    @State private var inputImage: UIImage?
+    @State private var image: Image?
     var body: some View {
             VStack(spacing: 40){
-                VStack{
-                    Image(uiImage: AccountProf.getProfPhoto() ?? UIImage(imageLiteralResourceName: "profile_photo_edit"))
-                    .frame(width: 40, height: 40)
+                ZStack{
+                    if image ==  nil {
+                        Image(uiImage: self.AccountProf.getUIImageFromCKAsset(image: self.AccountProf.user?.profilePhoto)!)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 150, height: 150)
+    //                    if AccountProf.username != nil{
+    //                        Text("\(AccountProf.username!)")
+    //                    }
+                    }
+                    else{
+                        image?
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 150, height: 150)
+                    }
                 }
                 List{
                     NavigationLink(destination: Text("Orders")) {
                         ProfileButton(imageName: "contact_us", label: "Order History")
                     }
-                    
+
                     NavigationLink(destination: Text("Reviews/Pics")) {
                         ProfileButton(imageName: "contact_us", label: "Restaurants Visted")
                     }
-                    
+
                     NavigationLink(destination: Text("Contact Us")) {
                         ProfileButton(imageName: "contact_us", label: "Contact Us")
                     }
-                    
+
                     NavigationLink(destination: Text("Report Problem")) {
                         ProfileButton(imageName: "report_problem", label: "Report Problem")
                     }
-                    
+
                     NavigationLink(destination: Text("Suggest Restaurant")) {
                         ProfileButton(imageName: "report_problem", label: "Suggest Restaurant")
                     }
-                    
+
                     NavigationLink(destination: Text("Sign Out")) {
                         ProfileButton(imageName: "report_problem", label: "Sign Out")
                     }
-                    
+
                 }
                 
             }.navigationBarTitle("Account Profile")
+            .navigationBarItems(trailing: Button(action: {
+                self.showingImagePicker.toggle()
+            }, label: {
+                    Text("Edit Profile Photo")
+        }))
+            .onAppear{
+                    if self.AccountProf.user == nil {
+                        print("calling fetching")
+                        self.AccountProf.fetchUser()
+                    }
+            }
+            .sheet(isPresented: $showingImagePicker, onDismiss: changePhoto){ ImagePicker(image: self.$inputImage)
+            }
+
         }
+    
+    func changePhoto(){
+        guard let inputImage = inputImage else { return }
+        image = Image(uiImage: inputImage)
+    }
     
 }
 
