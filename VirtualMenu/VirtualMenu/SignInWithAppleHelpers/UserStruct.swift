e09dea0c691425d1cbf5fe40adb7d8b03dfc1de8
RohoCodo <rtrocks722@gmail.com>
2020-06-20 14:32:54 -0700

    UserStruct

---
+++ VirtualMenu/VirtualMenu/SignInWithAppleHelpers/UserStruct.swift
@@ -0,0 +1,17 @@
+//
+//  UserStruct.swift
+//  VirtualMenu
+//
+//  Created by Rohan Tyagi on 6/18/20.
+//  Copyright © 2020 Rohan Tyagi. All rights reserved.
+//
+
+import Foundation
+
+struct UserStruct {
+    
+    static var name = "RongTan"
+    static var photo = "photo"
+    
+}
+
