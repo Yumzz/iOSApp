2cc38f76e9f2ffd6c7f7e34221a1bd795d9ea280
RohoCodo <rtrocks722@gmail.com>
2020-06-22 14:30:22 -0700

    Sign in

--- VirtualMenu/VirtualMenu/SignInWithAppleHelpers/SignInWithAppleDelegates.swift
+++ VirtualMenu/VirtualMenu/SignInWithAppleHelpers/SignInWithAppleDelegates.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import AuthenticationServices
+import CloudKit
 
 
 class SignInWithAppleDelegates: NSObject {
@@ -42,21 +43,82 @@ extension SignInWithAppleDelegates: ASAuthorizationControllerDelegate {
         switch authorization.credential {
         case let appleIdCredential as ASAuthorizationAppleIDCredential:
             if let _ = appleIdCredential.email, let _ = appleIdCredential.fullName {
+                KeychainItem.currentUserIdentifier = appleIdCredential.user
+                KeychainItem.currentUserFirstName = appleIdCredential.fullName?.givenName
+                KeychainItem.currentUserLastName = appleIdCredential.email
+                print("User Id - \(appleIdCredential.user)")
+                print("User Name - \(appleIdCredential.fullName?.description ?? "N/A")")
+                print("User Email - \(appleIdCredential.email ?? "N/A")")
+                print("Real User Status - \(appleIdCredential.realUserStatus.rawValue)")
                 registerNewAccount(credential: appleIdCredential)
+                //keychain
             } else {
+                KeychainItem.currentUserIdentifier = appleIdCredential.user
+                KeychainItem.currentUserFirstName = appleIdCredential.fullName?.givenName
+                KeychainItem.currentUserLastName = appleIdCredential.fullName?.familyName
+                KeychainItem.currentUserEmail = appleIdCredential.email
+                print("User Id - \(appleIdCredential.user)")
+                print("User Name - \(appleIdCredential.fullName?.description ?? "N/A")")
+                print("User Email - \(appleIdCredential.email ?? "N/A")")
+                print("Real User Status - \(appleIdCredential.realUserStatus.rawValue)")
                 signInWithExistingAccount(credential: appleIdCredential)
+                //keychain
             }
             
             break
             
         case let passwordCredential as ASPasswordCredential:
             signInWithUserAndPassword(credential: passwordCredential)
-            
+            //keychain
             break
             
         default:
-            break
+            let userID : String = KeychainItem.currentUserIdentifier!
+            
+            let query = CKQuery(recordType: "ARMUser", predicate: NSPredicate(value: true))
+            let db = CKContainer.default().publicCloudDatabase
+            var userExist = false
+            db.perform(query, inZoneWith: nil) { (records, error) in
+                for record in records! {
+                    userExist = userExist || (record.value(forKey: "CloudIdentifier") as! String == userID)
+                }
+            }
+            do {
+                sleep(1)
+            }
+            
+            if (!userExist) {
+                // add such a user
+                let ARMUserRecord = CKRecord(recordType: "ARMUser")
+                ARMUserRecord.setValue(userID, forKey: "CloudIdentifier")
+                ARMUserRecord.setValue(0, forKey: "Karma")
+                ARMUserRecord.setValue([], forKey: "ModelUploaded")
+                ARMUserRecord.setValue(nil, forKey: "ProfilePhoto")
+                ARMUserRecord.setValue([], forKey: "Reviews")
+                
+                db.save(ARMUserRecord){ (record, error) -> Void in
+                    DispatchQueue.main.sync {
+                        self.processResponse(record: record, error: error)
+                    }
+                }
+            }
+            UserStruct.name = userID
+            // 97 -124
         }
+    }
+    
+    private func processResponse(record: CKRecord?, error: Error?) {
+        var message = ""
+        
+        if let error = error {
+            print(error)
+            message = "We were not able to create a user."
+            
+        } else if record == nil {
+            message = "We were not able to create a user."
+        }
+        
+        print(message)
     }
     
     func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
