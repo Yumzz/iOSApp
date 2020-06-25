452c1c21fd5048fad1186fd3213218785c5e1741
RohoCodo <rtrocks722@gmail.com>
2020-06-25 12:47:15 -0700

    Login

--- VirtualMenu/VirtualMenu/LoginOrSignUpView.swift
+++ VirtualMenu/VirtualMenu/LoginOrSignUpView.swift
@@ -7,3 +7,56 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct LoginOrSignUpView: View {
+    
+    var body: some View {
+        NavigationView {
+            
+            VStack(spacing: 10){
+                
+                //Logo
+
+                Spacer()
+                    .frame(height: 10)
+                CustomButton(action: {
+//                    navigate to sign up page
+
+                }){
+                    HStack{
+                        Text("SIGN UP")
+                        .foregroundColor(.green)
+                    }
+                }
+                CustomButton(action: {
+//                    navigate to login page
+
+                }){
+                    HStack{
+                        Text("LOGIN")
+                        .foregroundColor(.green)
+                    }
+                }
+                
+                Spacer()
+                
+                
+                
+            }
+            
+            
+        }
+        
+        
+        
+    }
+    
+}
+
+
+struct LoginOrSignUpView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginOrSignUpView()
+    }
+}
