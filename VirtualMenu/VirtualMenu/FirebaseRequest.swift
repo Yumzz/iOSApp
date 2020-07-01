7c39002d81368ba29cc5919c266901cdc7a393f3
EageRAssassin <rt389@cornell.edu>
2020-06-30 23:35:57 -0700

    firebase initialized

---
+++ VirtualMenu/VirtualMenu/FirebaseRequest.swift
@@ -0,0 +1,18 @@
+//
+//  FirebaseRequest.swift
+//  VirtualMenu
+//
+//  Created by EageRAssassin on 6/30/20.
+//  Copyright © 2020 Rohan Tyagi. All rights reserved.
+//
+
+import Foundation
+//import Firebase
+
+class FirebaseRequest {
+    
+    init() {
+//        FirebaseApp.configure()
+//        let rootRef = Database.database().reference()
+    }
+}
