f0e28117ed89c35549598a9408fa01ff8e8c6bfb
EageRAssassin <rt389@cornell.edu>
2020-07-05 01:26:38 -0700

    database setup completed for fetch all dishes

--- VirtualMenu/VirtualMenu/AppDelegate.swift
+++ VirtualMenu/VirtualMenu/AppDelegate.swift
@@ -17,6 +17,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        
+        // test purposes only
+        
+//        let fr = FirebaseRequest()
+//        print("Firebase connection established")
+//        print(fr.fetchAllDishes())
         return true
     }
 
--- VirtualMenu/VirtualMenu/FirebaseRequest.swift
+++ VirtualMenu/VirtualMenu/FirebaseRequest.swift
@@ -7,12 +7,49 @@
 //
 
 import Foundation
-//import Firebase
+import Firebase
+
 
 class FirebaseRequest {
-    
+
+    let db = Firestore.firestore()
+        
     init() {
-//        FirebaseApp.configure()
-//        let rootRef = Database.database().reference()
+        
     }
+    
+    func fetchAllDishes() -> [DishFB] {
+        var dishList : [DishFB] = []
+        
+        db.collectionGroup("Dish").getDocuments { (querySnapshot, error) in
+            if let error = error {
+                print("Error getting documents: \(error)")
+            } else {
+                for document in querySnapshot!.documents {
+                    print("\(document.documentID) => \(document.data())")
+                    dishList.append(DishFB(snapshot: document)!)
+                }
+            }
+        }
+            
+        return dishList
+    }
+    
+    ///fetchDish() will fetch the dish given id in the database
+    ///- Parameters:
+    ///     - id : the id of the dish
+    ///- Returns: the Dish in the database
+//    func fetchDish(recordID : String) -> Dish {
+//
+    //    let docRef = db.collection("cities").document("SF")
+    //
+    //    docRef.getDocument { (document, error) in
+    //        if let document = document, document.exists {
+    //            let dataDescription = document.data().map(String.init(describing:)) ?? "nil"
+    //            print("Document data: \(dataDescription)")
+    //        } else {
+    //            print("Document does not exist")
+    //        }
+    //    }
+//    }
 }
