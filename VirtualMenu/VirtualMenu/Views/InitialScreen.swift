e1322494bf0ec332507156e9b1f8270f5f2f90cf
Valentin P <valentin.porcellini@gmail.com>
2020-07-11 16:10:27 -0700

    Fixed pagination

--- VirtualMenu/VirtualMenu/Views/InitialScreen.swift
+++ VirtualMenu/VirtualMenu/Views/InitialScreen.swift
@@ -9,62 +9,56 @@ import SwiftUI
 
 struct InitialScreen: View {
     
-    @State private var isShowingLoginSheet = false
-    @State private var isShowingSignUpSheet = false
-    
-        
     var body: some View {
         
         NavigationView{
             
             GeometryReader { geometry in
-            ZStack {
-                Image("initial_screen_back")
-                    .resizable()
-                    .overlay(Color(UIColor().colorFromHex("#F88379", 0.5)))
-                    .aspectRatio(geometry.size, contentMode: .fill)
-                    .edgesIgnoringSafeArea(.all)
+                ZStack {
+                    Image("initial_screen_back")
+                        .resizable()
+                        .aspectRatio(geometry.size, contentMode: .fill)
+                        .overlay(Color(UIColor().colorFromHex("#F88379", 0.5)))
+                        .edgesIgnoringSafeArea(.all)
                     
-                    
-            VStack{
-                
-                VStack{
-                    
-                    Text("Yumzz")
-                        .font(.custom("Montserrat-Bold", size: 48))
-                        .foregroundColor(Color.white)
-                        .bold()
+                    VStack{
+                        VStack{
+                            Text("Yumzz")
+                                .font(.custom("Montserrat-Bold", size: 48))
+                                .foregroundColor(Color.white)
+                                .bold()
+                            
+                            Spacer()
+                                .frame(height: (UIScreen.main.bounds.height/2.5))
+                        }
                         
-                    Spacer()
-                        .frame(height: (UIScreen.main.bounds.height/2.5))
-                }
-                      
-                VStack{
-                    Button(action: { self.isShowingSignUpSheet.toggle()})
-                    {
-                        Text("SIGN UP")
-                            .foregroundColor(Color(UIColor().colorFromHex("#FFFFFF", 1)))
+                        VStack{
+                            Button(action: {
+                            }, label: {
+                                NavigationLink(destination: SignUpView()) {
+                                    Text("SIGN UP")
+                                        .foregroundColor(Color(UIColor().colorFromHex("#FFFFFF", 1)))
+                                }
+                            })
+                                .cornerRadius(10)
+                                .padding().frame(width: UIScreen.main.bounds.width/1.5, height: 40)
+                                .background(Color(UIColor().colorFromHex("#000000", 1)))
+                            
+                            Spacer().frame(height: 30)
+                            
+                            Button(action: {
+                            }, label: {
+                                NavigationLink(destination: LoginView()) {
+                                    Text("LOGIN")
+                                        .foregroundColor(Color(UIColor().colorFromHex("#000000", 1)))
+                                }
+                            })
+                                .cornerRadius(10)
+                                .padding().frame(width: UIScreen.main.bounds.width/1.5, height: 40)
+                                .background(Color(UIColor().colorFromHex("#FFFFFF", 1)))
+                        }
                     }
-                    .cornerRadius(10)
-                    .padding().frame(width: UIScreen.main.bounds.width/1.5, height: 40)
-                    .background(Color(UIColor().colorFromHex("#000000", 1)))
-                    .sheet(isPresented: self.$isShowingSignUpSheet) { SignUpView() }
-                    
-                    Spacer().frame(height: 30)
-                    
-                    Button(action: { self.isShowingLoginSheet.toggle()})
-                    {
-                        Text("LOGIN")
-                            .foregroundColor(Color(UIColor().colorFromHex("#000000", 1)))
-                    }
-                    .cornerRadius(10)
-                    .padding().frame(width: UIScreen.main.bounds.width/1.5, height: 40)
-                    .background(Color(UIColor().colorFromHex("#FFFFFF", 1)))
-                    .sheet(isPresented: self.$isShowingLoginSheet) { LoginView() }
-                    
                 }
-            }
-        }
             }
         }
         
