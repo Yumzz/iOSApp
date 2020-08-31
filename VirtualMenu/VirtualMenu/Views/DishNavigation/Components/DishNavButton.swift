c45b107ab89eeda1e0603512cf9eb865c99093f5
RohoCodo <66083872+RohoCodo@users.noreply.github.com>
2020-08-31 15:05:45 -0700

    Update DishNavButton.swift

--- VirtualMenu/VirtualMenu/Views/DishNavigation/Components/DishNavButton.swift
+++ VirtualMenu/VirtualMenu/Views/DishNavigation/Components/DishNavButton.swift
@@ -2,8 +2,30 @@
 //  DishNavButton.swift
 //  VirtualMenu
 //
-//  Created by Rohan Tyagi on 8/31/20.
+//  Created by Rohan Tyagi on 8/28/20.
 //  Copyright © 2020 Rohan Tyagi. All rights reserved.
 //
 
 import Foundation
+import SwiftUI
+
+struct DishNavButton: View {
+    
+    var strLabel: String
+    
+    var body: some View {
+        Group {
+            Text(strLabel)
+            .foregroundColor(
+                Color(
+                    UIColor().colorFromHex("#707070", 1)
+                )
+            )
+            .padding()
+            .frame(width: UIScreen.main.bounds.width/2, height: 40)
+        }
+            .cornerRadius(10)
+            .background(Color(UIColor().colorFromHex("#FFFFFF", 1)))
+            .shadow(radius: 5)
+    }
+}
