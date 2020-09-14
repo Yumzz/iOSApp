6c5eb77d27d9d4e8113d446dabe914fd311f22ad
williambaibai <williambaibai@gmail.com>
2020-09-13 23:35:45 -0700

    Fix bottomsheet

--- VirtualMenu/VirtualMenu/Utils/BottomSheetView.swift
+++ VirtualMenu/VirtualMenu/Utils/BottomSheetView.swift
@@ -16,6 +16,7 @@ fileprivate enum Constant {
 
 struct BottomSheetView<Content: View>: View {
     @Binding var isOpen: Bool
+    @Binding var display: Bool
 
     let maxHeight: CGFloat
     let minHeight: CGFloat
@@ -38,43 +39,53 @@ struct BottomSheetView<Content: View>: View {
         }
     }
 
-    init(isOpen: Binding<Bool>, maxHeight: CGFloat, @ViewBuilder content: () -> Content) {
+    init(isOpen: Binding<Bool>, display: Binding<Bool>, maxHeight: CGFloat, @ViewBuilder content: () -> Content) {
         self.minHeight = maxHeight * Constant.minHeightRatio
         self.maxHeight = maxHeight
         self.content = content()
         self._isOpen = isOpen
+        self._display = display
     }
 
     var body: some View {
         GeometryReader { geometry in
-            VStack(spacing: 0) {
-                self.indicator.padding()
-                self.content
-            }
-            .frame(width: geometry.size.width, height: self.maxHeight, alignment: .top)
-            .background(Color(UIColor().colorFromHex("#F88379", 1)))
-            .cornerRadius(Constant.radius)
-            .frame(height: geometry.size.height, alignment: .bottom)
-            .offset(y: max(self.offset + self.translation, 0))
-            .animation(.interactiveSpring())
-            .gesture(
-                DragGesture().updating(self.$translation) { value, state, _ in
-                    state = value.translation.height
-                }.onEnded { value in
-                    let snapDistance = self.maxHeight * Constant.snapRatio
-                    guard abs(value.translation.height) > snapDistance else {
-                        return
-                    }
-                    self.isOpen = value.translation.height < 0
+            if self.display {
+                VStack(spacing: 0) {
+                    self.indicator.padding()
+                    self.content
                 }
-            )
+                .frame(width: geometry.size.width, height: self.maxHeight, alignment: .top)
+                .background(Color.white)
+                .cornerRadius(Constant.radius)
+                .frame(height: geometry.size.height, alignment: .bottom)
+                .offset(y: max(self.offset + self.translation, 0))
+                .animation(.interactiveSpring())
+                .gesture(
+                    DragGesture().updating(self.$translation) { value, state, _ in
+                        state = value.translation.height
+                    }.onEnded { value in
+                        if value.translation.height >= self.maxHeight / 1.5 || (!self.isOpen && value.translation.height >= self.minHeight / 2) {
+                            withAnimation {
+                                self.display.toggle()
+                                self.isOpen = true
+                            }
+                            return
+                        }
+                        let snapDistance = self.maxHeight * Constant.snapRatio
+                        guard abs(value.translation.height) > snapDistance else {
+                            return
+                        }
+                        self.isOpen = value.translation.height < 0
+                    }
+                )
+            }
         }
     }
 }
 
 struct BottomSheetView_Previews: PreviewProvider {
     static var previews: some View {
-        BottomSheetView(isOpen: .constant(false), maxHeight: 600) {
+        BottomSheetView(isOpen: .constant(false), display: .constant(true), maxHeight: 600) {
             Rectangle().fill(Color.red)
         }.edgesIgnoringSafeArea(.all)
     }
--- VirtualMenu/VirtualMenu/Views/RestaurantSearchView/RestaurantMapView/RestaurantMapView.swift
+++ VirtualMenu/VirtualMenu/Views/RestaurantSearchView/RestaurantMapView/RestaurantMapView.swift
@@ -14,7 +14,7 @@ struct RestaurantMapView: View {
     @State private var restaurants: [RestaurantFB] = [RestaurantFB]()
     @State var restChosen: RestaurantFB? = nil
     @State var isClicked = false
-    @State private var bottomSheetShown = false
+    @State private var bottomSheetOpen = true
     
     @State var region: [MKCoordinateRegion] = [MKCoordinateRegion]()
     
@@ -96,19 +96,17 @@ struct RestaurantMapView: View {
                         
                     .frame(maxWidth: .infinity)
                     .padding(.bottom, 25)
-//                    if (self.click.isClicked) {
-//                        BottomSheetView(isOpen: self.$bottomSheetShown, maxHeight: geometry.size.height*0.8) {
-//                            if self.click.restChosen != nil {
-//                                MapViewBottomView(restChosen: self.click.restChosen!)
-//                            }
-//                        }
-//                    }
                     
-                    .overlay(
-                        BottomSheetModal(display: self.$click.isClicked, backgroundColor: .constant(Color(UIColor().colorFromHex("#FFFFFF", 1))), rectangleColor: .constant(Color(UIColor().colorFromHex("#656565", 1)))) {
+                    BottomSheetView(isOpen: self.$bottomSheetOpen, display: self.$click.isClicked, maxHeight: geometry.size.height*0.8) {
+                        if self.click.restChosen != nil {
                             MapViewBottomView(restChosen: self.click.restChosen!)
                         }
-                    )
+                    }
+//                    .overlay(
+//                        BottomSheetModal(display: self.$click.isClicked, backgroundColor: .constant(Color(UIColor().colorFromHex("#FFFFFF", 1))), rectangleColor: .constant(Color(UIColor().colorFromHex("#656565", 1)))) {
+//                            MapViewBottomView(restChosen: self.click.restChosen!)
+//                        }
+//                    )
                 }
             }
         }
