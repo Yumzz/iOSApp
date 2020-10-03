087b3b4c6c6e38e900aec54879be232856906dc3
williambaibai <williambaibai@gmail.com>
2020-10-03 11:04:53 -0400

    Add App Clip

---
+++ VirtualMenu/YumzzAppClip/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  YumzzAppClip
+//
+//  Created by William Bai on 9/28/20.
+//  Copyright © 2020 Rohan Tyagi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
